9e275484cc9ce4a7d99c8d0db0ff4406fc4fe586
Adi Levy <levyadi93@gmail.com>
2021-02-21 00:47:46 +0200

    week 4 - exercises

--- Ex2.4.js
+++ Ex2.4.js
@@ -1,9 +1,7 @@
-const findUniq = (array) => array
-.sort((a,b) => a - b)
-.filter((number,i,array) => number !== array[0]);
+const findUniq = (array) => {
+array.sort((a,b) => a - b)
+return array[0] === array[1] ? array.pop() : array[0]
+};
 
-//tests 
-console.log(findUniq([1, 2, 1]));
 console.log(findUniq([0, 0, 0.55, 0, 0]));
 
-//TODO check for adding an edge case of all equals
